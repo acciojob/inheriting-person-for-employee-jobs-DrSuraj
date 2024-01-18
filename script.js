9c61a23a31c11d48d6cb4c4024f94afba95dcd9b
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2024-01-18 14:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
+// Person constructor function
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
+// Add greet method to Person prototype
 Person.prototype.greet = function () {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
+// Employee constructor function
 function Employee(name, age, jobTitle) {
-  // Call the Person constructor to set name and age
+  // Call the Person constructor to initialize name and age
   Person.call(this, name, age);
-
+  
   this.jobTitle = jobTitle;
 }
 
-// Inherit the Person prototype
+// Inherit the Person prototype in the Employee prototype
 Employee.prototype = Object.create(Person.prototype);
 
-// Add a method to the Employee prototype
+// Add jobGreet method to Employee prototype
 Employee.prototype.jobGreet = function () {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
-// Do not change the code below this line
-window.Person = Person;
-window.Employee = Employee;
+// Example usage
+const person = new Person("John", 25);
+person.greet();
+
+const employee = new Employee("Jane", 30, "Software Engineer");
+employee.greet(); // Inherited from Person
+employee.jobGreet(); // Added to Employee
